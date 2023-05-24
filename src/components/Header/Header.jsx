8ee22c4fbdd365e06749d98066a11f1a4ce44286
Chris Hollis <chrisbhollis@gmail.com>
2023-05-23 18:34:28 -0700

    setup initial App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header-container">
+        <p>Fake Store</p>
+        <p>Cart logo</p>
+    </div>
+  )
+}
+
+export default Header
